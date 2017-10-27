31953f2a1071d187f1ef4c6492f56ed46493efd0
purerosefallen <78877@qq.com>
2017-10-28 06:25:20 +0800

    setcode

--- script/c37564431.lua
+++ script/c37564431.lua
@@ -2,6 +2,7 @@
 local m=37564431
 local cm=_G["c"..m]
 xpcall(function() require("expansions/script/c37564765") end,function() require("script/c37564765") end)
+cm.Senya_name_with_prism=true
 function cm.initial_effect(c)
 	Senya.AddXyzProcedureCustom(c,function(c) return c:IsCode(m-5) end,nil,1,1)
 	c:SetSPSummonOnce(m)	
